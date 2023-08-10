2d33bc9efa776dc35993e0a9fba4d45245c9b34f
viktor7ltz <78755786+viktor7ltz@users.noreply.github.com>
2023-08-10 14:59:13 +0200

    fixed deprecated

--- acf-icon-picker.php
+++ acf-icon-picker.php
@@ -12,30 +12,35 @@ GitHub Plugin URI: https://github.com/houke/acf-icon-picker
 GitHub Branch: master
 */
 
-if( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) )
+	exit;
 
-if( !class_exists('acf_plugin_icon_picker') ) :
+if ( ! class_exists( 'acf_plugin_icon_picker' ) ) :
 
-class acf_plugin_icon_picker {
-
-	function __construct() {
-
-		$this->settings = array(
-			'version'	=> '1.9.1',
-			'url'		=> plugin_dir_url( __FILE__ ),
-			'path'		=> plugin_dir_path( __FILE__ )
+	class acf_plugin_icon_picker {
+		private static $settings = array(
+			'version' => '1.9.1',
+			'url'     => '',
+			'path'    => ''
 		);
 
-		add_action('acf/include_field_types', 	array($this, 'include_field_types'));
+		function __construct() {
+
+			self::$settings['url']  = plugin_dir_url( __FILE__ );
+			self::$settings['path'] = plugin_dir_path( __FILE__ );
+
+			add_action( 'acf/include_field_types', array( $this, 'include_field_types' ) );
+
+		}
+
+
+
+		function include_field_types( $version = false ) {
+			include_once( 'fields/acf-icon-picker-v5.php' );
+		}
 
 	}
 
-	function include_field_types( $version = false ) {
-		include_once('fields/acf-icon-picker-v5.php');
-	}
-
-}
-
-new acf_plugin_icon_picker();
+	new acf_plugin_icon_picker();
 
 endif;
